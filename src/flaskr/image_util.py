2332a57b5ec4c5a3e4215bc612bef3dae6f4d1c1
ryo0210 <yone0217@icloud.com>
2022-11-14 23:47:32 +0900

    ぼかしの方法を変更

--- src/flaskr/image_util.py
+++ src/flaskr/image_util.py
@@ -4,17 +4,16 @@ import cv2
 
 def read_image(file_path):
     img = Image.open(file_path)
-    scale = 1080 / max(img.size)
-    w = round(img.width * scale)
-    h = round(img.height * scale)
-    img = img.resize((w, h))
+    # scale = 1080 / max(img.size)
+    # w = round(img.width * scale)
+    # h = round(img.height * scale)
+    # img = img.resize((w, h))
     
     try:
         for orientation in ExifTags.TAGS.keys():
             if ExifTags.TAGS[orientation] == 'Orientation':
                 break
         exif = img._getexif()
-        print(exif)
         if exif is not None:
             if exif[orientation] == 3:
                 img = img.rotate(180, expand=True)
@@ -25,30 +24,29 @@ def read_image(file_path):
     except Exception as e:
         print(e)
 
-    if img.mode == 'RGBA':
-        img = img.convert('RGB')
-    return img
+    return pil2cv(img)
 
-def blur_image(img, positions):
-    "positions: [[左上, 右上], ...]"
-    blurred_image = img.filter(ImageFilter.GaussianBlur(10))
+def pil2cv(img):
+    mode = img.mode
+    if mode == '1':
+        return np.array(img, dtype=np.uint8) * 255
+        # return cv_img
+    elif mode == 'RGB':
+        cv_img = np.array(img, dtype=np.uint8)
+        return cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+    elif mode == 'P' or mode == 'CMYK':
+        cv_img = np.array(img.convert('RGB'), dtype=np.uint8)
+        return cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+    elif mode == 'RGBA':
+        cv_img = np.array(img, dtype=np.uint8)
+        return cv2.cvtColor(cv_img, cv2.COLOR_RGBA2BGR)
+    else:
+        raise ValueError(f'unhandled image color mode: {mode}')
 
-    masked_img = Image.new("L", img.size)
-    draw = ImageDraw.Draw(masked_img)
 
-    for position in positions:
-        draw.rectangle([position[0], position[1]], fill=255)
-        # draw.polygon(ocr_result["position"], fill=255)
-
-    img.paste(blurred_image, (0, 0), masked_img)
-    return img
-
-def blur_image_on_cv2(img, positions):
-    blur_img = cv2.GaussianBlur(img, (99, 99), 0)
-
-    masked_img = np.zeros_like(img)
-    for position in positions:
-        cv2.rectangle(masked_img, position[0], position[1], (255, 255, 255), -1)
-    transparence = (0,0,0)
-    front = cv2.bitwise_and(blur_img, masked_img)
-    return np.where(front==transparence, img, front)
+def blur_image(src, mask_img):
+    blurred_image = cv2.GaussianBlur(src, (99, 99), 0)
+    transparency = (0, 0, 0)
+    front = cv2.bitwise_and(blurred_image, mask_img)
+    return np.where(front==transparency, src, front)
+    
--- src/flaskr/ocr_test.py
+++ src/flaskr/ocr_test.py
@@ -6,31 +6,31 @@ import easyocr
 import numpy as np
 from PIL import Image, ImageDraw, ImageFilter
 
-import flaskr.image_util as iu
+import image_util as iu
+# import flaskr.image_util as iu
 
 
 def main():
-    img_path = "./image/word/IMG_4646.JPG"
-    # img_path = "./image/scenery.jpg"
+    img_path = "IMG_4670.JPG"
     img_name = os.path.splitext(img_path)[0]
 
     img = iu.read_image(img_path)
     ocr_results = ocr(img)
-    result_img = blur_image(img, ocr_results)
+    result_img = blur(img, ocr_results)
 
-    result_img.save(f"{img_name}_ocr_result.png")
+    cv2.imwrite(f"{img_name}_ocr_result.png", result_img)
 
 
 def ocr(img):
     reader = easyocr.Reader(['ja','en'])
-    results = reader.readtext(np.array(img))
+    results = reader.readtext(img)
     if not results:
         print("文字が検出されませんでした。")
 
     ocr_results = []
     for result in results:
         ocr_result = {
-            "position" : (
+            "points" : (
                 (int(result[0][0][0]), int(result[0][0][1])),
                 (int(result[0][1][0]), int(result[0][1][1])),
                 (int(result[0][2][0]), int(result[0][2][1])),
@@ -39,20 +39,20 @@ def ocr(img):
             "text"     : result[1],
             "confident": result[2]
         }
-        print(ocr_result)
         ocr_results.append(ocr_result)
     return ocr_results
 
-def set_coordinates(ocr_results):
-    coordinates = []
+
+def generate_mask_image(img, ocr_results):
+    mask_img = np.zeros_like(img)
     for ocr_result in ocr_results:
-        coordinates.append([ocr_result["position"][0], ocr_result["position"][2]])
-    return coordinates
+        cv2.fillConvexPoly(mask_img, np.array(ocr_result['points']), (255, 255, 255))
+    return mask_img
 
+def blur(img, ocr_results):
+    mask_img = generate_mask_image(img, ocr_results)
+    return iu.blur_image(img, mask_img)
 
-def blur_image(img, ocr_results):
-    coordinates = set_coordinates(ocr_results)
-    return iu.blur_image(img, coordinates)
 
 
 if __name__ == "__main__":
